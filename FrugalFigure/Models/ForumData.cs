1cc82e5e143107956d749521557f9b49aec3ce08
Evers P <evers@Everss-MBP.attlocal.net>
2022-11-15 17:32:03 -0500

    Updated Version

---
+++ FrugalFigure/Models/ForumData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FrugalFigure.Models
+{
+    public abstract class data
+    {
+        public string name { get; set; }
+        public double amount { get; set; }
+    }
+
+    public class Needs :data
+    {
+
+    }
+    public class Wants : data
+    {
+
+    }
+    public class Savings : data
+    {
+
+    }
+}
+
--- FrugalFigure/wwwroot/js/site.js
+++ FrugalFigure/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var typing = new Typed(".text", {
+    Typed : true,
+    strings: ["Frugal", "Thrifty", "Canny", "Saving"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+});
